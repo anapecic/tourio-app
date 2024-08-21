10751fdb84abd43301d9ef1740fe84476bc25290
Ana Pecic <ana.pecic97@gmail.com>
2024-08-21 11:49:34 +0200

    fixed deployment issue

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -2,7 +2,7 @@ import dbConnect from "@/db/connect";
 import Place from "@/db/models/Place";
 
 export default async function handler(request, response) {
-  dbConnect();
+  await dbConnect();
   if (request.method === "GET") {
     const places = await Place.find();
     response.status(200).json(places);
